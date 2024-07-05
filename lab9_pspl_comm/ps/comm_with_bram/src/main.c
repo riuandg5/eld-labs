4e4d9068a38e57695c886d984d189345e00819a9
Rishu Anand <riuandg5@gmail.com>
2024-07-05 07:34:33 +0530

    redo lab9 bram_comm

--- lab9_pspl_comm/ps/comm_with_bram/src/main.c
+++ lab9_pspl_comm/ps/comm_with_bram/src/main.c
@@ -1,50 +1,56 @@
-#include <stdio.h>
 #include "platform.h"
 #include "xparameters.h"
-#include "xil_io.h"
-#include "xil_cache.h"
 #include "xil_printf.h"
-
+#include "xil_io.h"
 
 int main()
 {
+    // Initialize the platform (sets up the hardware and necessary libraries)
     init_platform();
 
-    // input data array
-    int data_in[] = {4, 9, 16, 8, 36, 49, 64, 81, 100, 121};
-    // length of input data array
-    // (4*10)/4, as int is of 4 Bytes
-    int total_data = sizeof(data_in)/sizeof(data_in[0]);
+    // Define the input data array with hardcoded entries
+    u32 data_in[] = {4, 9, 16, 8, 36, 49, 64, 81, 100, 121};
+    // Calculate the number of elements in the input data array
+    u32 data_size = sizeof(u32);
+    u32 total_data = sizeof(data_in) / data_size;
 
-    // base address of bram
-    int base_addr = XPAR_AXI_BRAM_CTRL_0_S_AXI_BASEADDR;
-    // size of each address in bits
-    int addr_size = 8; // 1 Byte address
+    // Store the base address of BRAM
+    UINTPTR bram_base_addr = XPAR_BRAM_0_BASEADDR;
+    // Define address size >= data size
+    u32 addr_size = data_size;
 
-    printf("Start writing data to bram\n\r");
-    // write input data to bram
-    for(int i = 0; i < total_data; i++) {
-    	// calculate current address as base address + offset * address size
-    	int curr_addr = base_addr + i * addr_size;
+    // Variable to store calculated current address
+    UINTPTR curr_addr;
 
-    	// write 4*8 bits input data at current address
-    	Xil_Out32(curr_addr, data_in[i]);
-    	printf("index = %d, address = %d, data = %d\n", i, curr_addr, data_in[i]);
+    // Begin the process of writing data to BRAM
+    xil_printf("Start writing data to BRAM\n\r");
+    for (u32 i = 0; i < total_data; i++) {
+        // Calculate the current address as base address + offset
+        curr_addr = bram_base_addr + i * addr_size;
+
+        // Write a 4-byte (32-bit) integer from the data_in array to the calculated address in BRAM
+        Xil_Out32(curr_addr, data_in[i]);
+        xil_printf("index = %u, address = 0x%08X, data = %u\n", i, curr_addr, data_in[i]);
     }
-    printf("\nDone writing data to bram\n\r");
+    xil_printf("\nDone writing data to BRAM\n\r");
 
-    printf("Start reading data from bram\n\r");
-    // read data from bram
-	for(int i = 0; i < total_data; i++) {
-		// calculate current address as base address + offset * address size
-		int curr_addr = base_addr + i * addr_size;
+    // Variable to store data read
+    u32 data_out;
 
-		// read 4*8 bits data at current address
-		int data_out = Xil_In32(curr_addr);
-		printf("index = %d, address = %d, data = %d\n", i, curr_addr, data_out);
-	}
-	printf("\nDone reading data from bram\n\r");
+    // Begin the process of reading data back from BRAM
+    xil_printf("Start reading data from BRAM\n\r");
+    for (u32 i = 0; i < total_data; i++) {
+        // Calculate the current address as base address + offset
+        curr_addr = bram_base_addr + i * addr_size;
 
+        // Read a 4-byte (32-bit) integer from the calculated address in BRAM
+        data_out = Xil_In32(curr_addr);
+        xil_printf("index = %u, address = 0x%08X, data = %u\n", i, curr_addr, data_out);
+    }
+    xil_printf("\nDone reading data from BRAM\n\r");
+
+    // Cleanup the platform (frees up resources used by the hardware and libraries)
     cleanup_platform();
+
     return 0;
 }
